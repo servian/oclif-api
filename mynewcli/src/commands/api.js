7047449d1ac482057d066f45605421b950642887
Marat Levit <mlevit@gmail.com>
2021-12-08 13:57:08 +1100

    API cleanup

--- mynewcli/src/commands/api.js
+++ mynewcli/src/commands/api.js
@@ -9,28 +9,33 @@ class APICommand extends Command {
     try {
       var app = express();
       app.use(express.json());
-      app.listen(port, () => {
-        console.log(`API server running at http://localhost:${port}`);
-      });
+
+      app.get("/", (req, res) => {});
+
+      app.get("/favicon.ico", (req, res) => {});
+
+      app.get("/service-worker.js", (req, res) => {});
 
       app.get("/*", async (req, res) => {
-        if (req.path !== "/" && req.path !== "/favicon.ico") {
-          // Generate class path from route
-          var classPath = req.path.replace(/\/$/gm, "").slice(1);
+        // Generate class path from route
+        var classPath = req.path.replace(/\/$/gm, "").slice(1);
 
-          // Import command from class path
-          const commandClass = require(`./${classPath}.js`);
+        // Import command from class path
+        const commandClass = require(`./${classPath}.js`);
 
-          // Create command parameters
-          var parameters = ["--api"];
-          Object.keys(req.query).forEach(function (key) {
-            parameters.push(`--${key}`);
-            parameters.push(req.query[key]);
-          });
-
-          // Execute command
-          res.status(200).json(await commandClass.run(parameters));
+        // Create command parameters
+        var parameters = ["--api"];
+        for (var key in req.query) {
+          parameters.push("--" + key);
+          parameters.push(req.query[key]);
         }
+
+        // Execute command
+        res.status(200).json([await commandClass.run(parameters)]);
+      });
+
+      app.listen(port, () => {
+        console.log(`API server running at http://localhost:${port}`);
       });
     } catch (error) {
       // error handling here
